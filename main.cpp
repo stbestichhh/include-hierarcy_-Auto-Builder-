7b10be2035f24669ffa5d4b8cd46e4e639112642
Kiril Yakymchuk <stbestich@gmail.com>
2023-12-05 17:04:16 +0200

    Creating UML diagramm of hieracy

--- main.cpp
+++ main.cpp
@@ -5,10 +5,11 @@
 #include <unordered_set>
 #include <filesystem>
 #include <queue>
+#include <windows.h>
 
 namespace fs = std::filesystem;
 
-std::unordered_map<std::string, std::unordered_set<std::string>> dependencyMap; //storage for dependencies
+std::unordered_map<std::string, std::unordered_set<std::string>> dependencyMap;
 
 void processFile(const std::string &filePath)
 {
@@ -17,20 +18,19 @@ void processFile(const std::string &filePath)
 
   std::string currentFile = fs::path(filePath).filename().string();
 
-  while (std::getline(file, line)) //checks file for '#include...'
-  {    
+  while (std::getline(file, line))
+  {
+    // Простой пример: предполагаем, что зависимости указываются после ключевого слова '#include'
     size_t pos = line.find("#include");
     if (pos != std::string::npos)
     {
-      std::string dependency = line.substr(pos + 9); // '#include length = 8'
-
-      // extract dependency file name
+      std::string dependency = line.substr(pos + 9); // Длина "#include" равна 8
       dependency = dependency.substr(dependency.find_first_of("\"<") + 1);
       dependency = dependency.substr(0, dependency.find_last_of("\">"));
-      
-      // making a path to file 
+
+      // Добавляем зависимость только если файл существует в папке проекта
       fs::path dependencyPath = fs::path(filePath).parent_path() / dependency;
-      if (fs::exists(dependencyPath)) // if file exsists in directory, add it to dependency storage
+      if (fs::exists(dependencyPath))
       {
         dependencyMap[currentFile].insert(dependency);
       }
@@ -38,7 +38,7 @@ void processFile(const std::string &filePath)
   }
 }
 
-void processFilesInDirectory(const std::string &directoryPath) // finds .rc | .cpp | .h files and calling processFile for each
+void processFilesInDirectory(const std::string &directoryPath)
 {
   for (const auto &entry : fs::directory_iterator(directoryPath))
   {
@@ -58,10 +58,11 @@ void processFilesInDirectory(const std::string &directoryPath) // finds .rc | .c
   }
 }
 
-void printDependencyHierarchy(const std::string &mainFile, int level = 0)  //print hierary
+void printDependencyHierarchy(const std::string &mainFile, std::ofstream &outFile, int level = 0)
 {
-  if (dependencyMap.find(mainFile) == dependencyMap.end()) //if file not found in storage
-  {   
+  if (dependencyMap.find(mainFile) == dependencyMap.end())
+  {
+    std::cerr << "Главный файл не найден в зависимостях.\n";
     return;
   }
 
@@ -69,53 +70,65 @@ void printDependencyHierarchy(const std::string &mainFile, int level = 0)  //pri
 
   for (const auto &dependency : dependencies)
   {
-    for (int i = 0; i < level; ++i)
-    {
-      std::cout << "  "; // for spaces in hierarcy
-    }
-    std::cout << dependency << "\n";
-    
-    printDependencyHierarchy(dependency, level + 1);
+    outFile << std::string(level * 2, ' ') << "\"" << mainFile << "\" -> \"" << dependency << "\" [style=\"solid\", color=\"black\", shape=\"box\", dir=\"back\", label=\"#include\"]\n";
+
+    // Обработка зависимостей рекурсивно
+    printDependencyHierarchy(dependency, outFile, level + 1);
   }
 }
 
-void processUnlinkedFiles(const std::string &projectPath)
+void writeUmlToFile(const std::string &mainFile, const std::string &outputFileName)
 {
-  std::unordered_set<std::string> allFiles; // storage for all files in directory
-  std::queue<std::string> unlinkedFiles; 
+  std::ofstream outFile(outputFileName);
+  outFile << "digraph UMLDiagram {\n";
+  outFile << "node [shape=box];\n"; // Задаем форму узлов как квадраты
 
-  for (const auto &entry : fs::directory_iterator(projectPath)) // add files in general storage 'allFiles'
+  printDependencyHierarchy(mainFile, outFile);
+
+  outFile << "}\n";
+  outFile.close();
+
+  std::cout << "Иерархия зависимостей записана в файл " << outputFileName << std::endl;
+}
+
+std::string findDotExePath()
+{
+  char buffer[MAX_PATH];
+  if (SearchPathA(NULL, "dot.exe", NULL, MAX_PATH, buffer, NULL) != 0)
   {
-    if (entry.is_regular_file())
+    return buffer;
+  }
+  return ""; // Возвращаем пустую строку в случае ошибки
+}
+
+void convertDotToImage(const std::string &dotFileName, const std::string &outputImageName)
+{
+  std::string dotPath = findDotExePath();
+
+  if (!dotPath.empty())
+  {
+    // Создаем процесс dot.exe
+    STARTUPINFOA si = {sizeof(STARTUPINFO)};
+    PROCESS_INFORMATION pi;
+
+    std::string commandLine = dotPath + " -Tpng " + dotFileName + " -o " + outputImageName;
+    if (CreateProcessA(NULL, const_cast<char *>(commandLine.c_str()), NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
     {
-      std::string filePath = entry.path().string();
-      if (filePath.size() >= 2 &&
-          (filePath.compare(filePath.size() - 2, 2, ".h") == 0 || filePath.compare(filePath.size() - 4, 4, ".cpp") == 0 || filePath.compare(filePath.size() - 3, 3, ".rc") == 0))
-      {
-        allFiles.insert(fs::path(filePath).filename().string());
-      }
+      // Ждем завершения процесса
+      WaitForSingleObject(pi.hProcess, INFINITE);
+
+      // Закрываем дескрипторы процесса и потока
+      CloseHandle(pi.hProcess);
+      CloseHandle(pi.hThread);
+    }
+    else
+    {
+      std::cerr << "Ошибка при запуске dot.exe: " << GetLastError() << std::endl;
     }
   }
-
-  for (const auto &[file, dependencies] : dependencyMap) //deletes file from storage if it included somewhere
+  else
   {
-    allFiles.erase(file);
-    for (const auto &dependency : dependencies)
-    {
-      allFiles.erase(dependency);
-    }
-  }
-
-  for (const auto &unlinkedFile : allFiles) // add all files that stayed in 'allFiles'
-  {
-    unlinkedFiles.push(unlinkedFile);
-  }
-
-  std::cout << "Отдельные файлы, которые не включены нигде:\n";
-  while (!unlinkedFiles.empty())
-  {
-    std::cout << unlinkedFiles.front() << "\n";
-    unlinkedFiles.pop();
+    std::cerr << "dot.exe не найден.\n";
   }
 }
 
@@ -129,13 +142,15 @@ int main()
   std::cout << "Введите имя главного файла (с расширением, например, main.cpp): ";
   std::getline(std::cin, mainFile);
 
+  std::string dotFileName = "output.dot";
+  std::string outputImageName = "output.png";
+
   // Преобразование относительного пути в абсолютный
   fs::path absolutePath = fs::absolute(projectPath);
 
   processFilesInDirectory(absolutePath.string());
-  printDependencyHierarchy(mainFile);
-
-  processUnlinkedFiles(absolutePath.string());
+  writeUmlToFile(mainFile, dotFileName);
+  convertDotToImage(dotFileName, outputImageName);
 
   return 0;
 }
